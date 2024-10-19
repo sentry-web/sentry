be9ec4e613bc79062975e00a0b3fd3b13c8f48b5
sentry <sentry725@gmail.com>
2024-10-19 09:40:02 +0000

    update

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const fs = require("fs")
 const path = require("path")
 
 
-mongoose.connect(config.ConnnectionString);
+mongoose.connect(config.connectionString);
 
 const app = express();
 app.use(express.json());
